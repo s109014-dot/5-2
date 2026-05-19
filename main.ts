bce451a9b29af5f58c6c5733e6452e628df80da4
s109014-dot <s109014@kid.saes.tyc.edu.tw>
2026-05-19 15:23:35 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,10 @@
 basic.forever(function () {
-    pins.digitalWritePin(DigitalPin.P0, 1)
-    basic.pause(500)
     pins.digitalWritePin(DigitalPin.P0, 0)
-    basic.pause(100)
+    basic.pause(10000)
+    pins.digitalWritePin(DigitalPin.P0, 0)
+    pins.digitalWritePin(DigitalPin.P0, 0)
+    basic.pause(1500)
+    pins.digitalWritePin(DigitalPin.P0, 0)
+    pins.digitalWritePin(DigitalPin.P0, 0)
+    basic.pause(10000)
 })
